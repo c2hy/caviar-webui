66b21858122b00a1023fb05d69fdb150b0d42995
c2hy <c2hy@hotmail.com>
2021-05-01 23:10:09 +0800

    插入 console

--- vue.config.js
+++ vue.config.js
@@ -2,16 +2,13 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
-  devServer:{
-    host:"localhost",
-    port:"8080",
-    proxy:{
-      '/api':{
-        target: "http://localhost:3000/",
-        changOrigin:true,
-        pathRewrite:{
-          '^/api':'/' 
+  devServer: {
+    proxy: {
+      '/api': {
+        target: "http://192.168.1.6:3000/",
+        changOrigin: true,
+        pathRewrite: {
+          '^/api': '/'
         }
       }
     }
